b152ca9f5fd7ed947ff9ca5634395bc90826cdb5
Marcelo de Oliveira <marcelo_rde@yahoo.com.br>
2021-04-06 22:02:47 -0300

    Add files via upload

---
+++ operadoresAritmeticos.js
@@ -0,0 +1,28 @@
+/* ------  Operadores Aritméticos ------ */
+
+var a = 10;
+var b = a + 2; // b = 12s
+var c = a - 15; // c = -5
+var d = a * 3; // d = 30
+var e = a / 3; // e = 3.3333
+
+console.log(`A: ${a}`);
+console.log(`B: ${b}`);
+console.log(`C: ${c}`);
+console.log(`D: ${d}`);
+console.log(`E: ${e}`);
+
+/* ------  Outros Operadores Aritméticos ------ */
+console.log('-----------------');
+
+var n1 = 5;
+var n2 = n1**2; // potencia -> n2 = 25
+var n3 = n1 % 2; // modulo = resto da divisão n3 = 1
+var n4 = 10 - 6 / 2; // n4 = 7
+var n5 = (10 - 6) / 2; // n5 = 2
+
+console.log(`N1: ${n1}`);
+console.log(`N2: ${n2}`);
+console.log(`N3: ${n3}`);
+console.log(`N4: ${n4}`);
+console.log(`N5: ${n5}`);
